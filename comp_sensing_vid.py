29ecc009cb99a90b227e50e68ce62b6fff973b6c
kurtisdavid <kurtis.david@live.com>
2019-03-20 23:42:27 -0500

    add video EM algorithm, untested

---
+++ comp_sensing_vid.py
@@ -0,0 +1,87 @@
+import numpy as np
+import pickle as pkl
+import os
+import parser
+
+import torch
+from torchvision import datasets
+
+import utils
+import cs_dip
+import baselines as baselines 
+import time
+from pyr_utils import PyrDown
+
+NEW_RECONS = False
+
+args = parser.parse_args('configs.json')
+print(args)
+
+NUM_MEASUREMENTS_LIST, ALG_LIST = utils.convert_to_list(args)
+
+# WE ASSUME dataset folder is all frames of a video
+dataloader = utils.get_data(args) # get dataset of images
+
+for num_meas in NUM_MEASUREMENTS_LIST:
+    args.NUM_MEASUREMENTS = num_meas 
+    
+    # init measurement matrix
+    A = baselines.get_A(args.IMG_SIZE*args.IMG_SIZE*args.NUM_CHANNELS, args.NUM_MEASUREMENTS)
+    
+    net = None
+    z = None
+    gan_logger = []
+    z_logger = []
+    # evolutions
+    noisy = []
+    z_s = []
+    x_hats = []
+    for c, (batch, _, im_path) in enumerate(dataloader):
+        x = batch.view(1,-1).cpu().numpy() # define image
+        y = x @ A
+        for alg in ALG_LIST:
+            args.ALG = alg
+
+            if utils.recons_exists(args, im_path): # to avoid redundant reconstructions
+                continue
+            NEW_RECONS = True
+
+            if alg == 'csdip' and not args.GAUSSIAN_PYR:
+                estimator = cs_dip.dip_estimator(args)
+            elif alg == 'csdip' and args.GAUSSIAN_PYR:
+                module = PyrDown(channels = args.NUM_CHANNELS, n_levels = args.N_LEVELS])
+                z_estimator, gan_estimator  = cs_dip.vid_dip_estimator(args,module)
+                y = module(torch.from_numpy(np.reshape(x,(1,args.NUM_CHANNELS,args.IMG_SIZE,args.IMG_SIZE))).cuda()).view(-1,1).cpu()
+            elif alg == 'dct':
+                estimator = baselines.lasso_dct_estimator(args)
+            elif alg == 'wavelet':
+                estimator = baselines.lasso_wavelet_estimator(args)
+            elif alg == 'bm3d' or alg == 'tval3':
+                raise NotImplementedError('BM3D-AMP and TVAL3 are implemented in Matlab. \
+                                            Please see GitHub repository for details.')
+            else:
+                raise NotImplementedError
+            
+             
+            np.save('vid_noisy_' + str(c),y)
+           
+            # EM training, don't train z on first frame, need trained gan first... 
+            if net is not None:
+                z = z_estimator(net, z, A, y, args, z_logger)
+            net, z, x_hat = gan_estimator(A, y, args, gan_logger, net=net, z=z)
+            # z shouldn't change if net is not None, since that is not trainable in gan_estimator
+            z_s.append(z.data.cpu())
+            x_hats.append(x_hat)          
+                     
+            # utils.save_reconstruction(x_hat, args, im_path)
+    np.save('gan_losses',gan_logger)
+    np.save('z_losses',z_logger)
+    np.save('noisy',noisy)
+    np.save('z_s', z)
+    np.save('x_hats', x_hats)
+
+
+if NEW_RECONS == False:
+    print('Duplicate reconstruction configurations. No new data generated.')
+else:
+    print('Reconstructions generated!')
--- cs_dip.py
+++ cs_dip.py
@@ -17,6 +17,102 @@ BATCH_SIZE = 1
 EXIT_WINDOW = 51
 loss_re, recons_re = utils.init_output_arrays(args)
 
+# performs EM-like optimization, trading latent z training and GAN training.
+def vid_dip_estimator(args, custom=None):
+    def z_estimator(net, z_, A_val, y_batch_val, args, z_logger):
+        # make it trainable
+        z = Variable(z_.copy(), requires_grad=True)
+        z_param = torch.nn.Parameter(z)
+        optim = torch.optim.SGD([z_param], lr=0.01, momentum=0.9)
+        if CUDA:
+            net.cuda()
+        z_iter = []
+        loss_iter = [] 
+        for i in range(args.z_NUM_ITER):
+            optim.zero_grad()
+            G = net(z)
+            if custom is None:
+                AG = torch.matmul(G.view(BATCH_SIZE,-1),A) # A*G(z)
+            else:
+                AG = custom(G).view(-1,1)
+            y_loss = torch.mean(torch.sum(se(AG,y),dim=1))
+            # calculate total variation loss 
+            tv_loss = (torch.sum(torch.abs(G[:,:,:,:-1] - G[:,:,:,1:]))\
+                    + torch.sum(torch.abs(G[:,:,:-1,:] - G[:,:,1:,:]))) 
+
+            total_loss = y_loss + tvc*tv_loss               
+            # stopping condition to account for optimizer convergence
+            if i >= args.NUM_ITER - EXIT_WINDOW: 
+                z_iter.append(z.data.cpu().numpy())
+                loss_iter.append(total_loss.data.cpu().numpy())
+
+            total_loss.backward() # backprop
+            optim.step() # only update the latent vector
+        z_logger.append(loss_iter)
+        idx_re = np.argmin(loss_iter,axis=0)
+        z_hat = recons_iter[idx_re]
+
+        return z_hat
+    def gan_estimator(A_val, y_batch_val, args, gan_logger, net=None, z=None):
+        y = torch.FloatTensor(y_batch_val).type(dtype) # init measurements y
+        A = torch.FloatTensor(A_val).type(dtype)       # init measurement matrix A
+
+        mu, sig_inv, tvc, lrc = utils.get_constants(args, dtype)
+        # should only be true for both at first iteration (train weights first...) 
+        if net is None:
+            net = utils.init_dcgan(args)
+        if z is None: 
+            z = torch.zeros(BATCH_SIZE*args.Z_DIM).type(dtype).view(BATCH_SIZE,args.Z_DIM,1,1)
+            z.data.normal_().type(dtype) #init random input seed
+        else:
+            z = z.copy()
+            z.requires_grad = False
+        
+        if CUDA:
+            net.cuda() # cast network to GPU if available
+    
+        optim = torch.optim.RMSprop(net.parameters(),lr=0.001, momentum=0.9, weight_decay=0)
+        loss_iter = []
+        recons_iter = []
+        
+        for i in range(args.NUM_ITER):
+
+            optim.zero_grad()
+
+            # calculate measurement loss || y - A*G(z) ||
+            G = net(z)
+            if custom is None:
+                AG = torch.matmul(G.view(BATCH_SIZE,-1),A) # A*G(z)
+            else:
+                AG = custom(G).view(-1,1)
+            y_loss = torch.mean(torch.sum(se(AG,y),dim=1))
+            # calculate total variation loss 
+            tv_loss = (torch.sum(torch.abs(G[:,:,:,:-1] - G[:,:,:,1:]))\
+                    + torch.sum(torch.abs(G[:,:,:-1,:] - G[:,:,1:,:]))) 
+
+                # calculate learned regularization loss
+#                layers = net.parameters()
+#                layer_means = torch.cat([layer.mean().view(1) for layer in layers])
+#                lr_loss = torch.matmul(layer_means-mu,torch.matmul(sig_inv,layer_means-mu))
+                
+#                total_loss = y_loss + lrc*lr_loss + tvc*tv_loss # total loss for iteration i
+            total_loss = y_loss + tvc*tv_loss               
+            # stopping condition to account for optimizer convergence
+            if i >= args.NUM_ITER - EXIT_WINDOW: 
+                recons_iter.append(G.data.cpu().numpy())
+                loss_iter.append(total_loss.data.cpu().numpy())
+            
+            total_loss.backward() # backprop
+            optim.step()
+        # to view later...
+        gan_logger.append(loss_iter)
+        
+        idx_re = np.argmin(loss_iter,axis=0)
+        x_hat = recons_iter[idx_re]
+        return net, z, x_hat
+    
+    return z_estimator, gan_estimator  
+
 def dip_estimator(args, custom=None):
     def estimator(A_val, y_batch_val, args):
 
@@ -60,7 +156,6 @@ def dip_estimator(args, custom=None):
                 
 #                total_loss = y_loss + lrc*lr_loss + tvc*tv_loss # total loss for iteration i
                 total_loss = y_loss + tvc*tv_loss               
-                total_loss = y_loss 
                 # stopping condition to account for optimizer convergence
                 if i >= args.NUM_ITER - EXIT_WINDOW: 
                     recons_iter.append(G.data.cpu().numpy())
